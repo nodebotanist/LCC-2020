ba421b95f6cd7659cfcddece2644ff40f2c1d48b
Mx. Kas Perch <the@nodebotani.st>
2020-06-19 16:46:38 -0500

    robot working with redis

--- robot-code/index.py
+++ robot-code/index.py
@@ -1,9 +1,12 @@
 import time
+import json
 import unicornhathd
 import redis
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
+r.set('colorQueue', json.dumps([{'r':123, 'g': 0, 'b':0}, {'r':0, 'g':0, 'b':100}]))
+
 while True:
-  print(r.get('colorQueue'))
-  sleep(5)
+  print(json.loads(r.get('colorQueue')))
+  time.sleep(5)
