759e894d62d3a46c92cf3c4425cc18890779408e
Mx. Kas Perch <the@nodebotani.st>
2020-06-19 16:55:50 -0500

    robot code complete

--- robot-code/index.py
+++ robot-code/index.py
@@ -5,8 +5,22 @@ import redis
 
 r = redis.Redis(host='localhost', port=6379, db=0)
 
-r.set('colorQueue', json.dumps([{'r':123, 'g': 0, 'b':0}, {'r':0, 'g':0, 'b':100}]))
-
 while True:
-  print(json.loads(r.get('colorQueue')))
+  color_queue = r.get('colorQueue')
+  if color_queue == None:
+    color_queue = []
+  else:
+    color_queue = json.loads(color_queue)
+  print(color_queue)
+  x = 0
+  y = 0
+  index = 0
+  for color in color_queue:
+    unicornhathd.set_pixel(x, y, color['r'], color['g'], color['b'])
+    x += 1
+    index +=1
+    if x > 15:
+      x = 0
+      y += 1
+  unicornhathd.show()
   time.sleep(5)
